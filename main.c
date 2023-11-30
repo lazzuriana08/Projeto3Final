8123940e4c1b582085c9215665bf2ecf6eb24907
Ana Carolina Lazzuri <148160943+lazzuriana08@users.noreply.github.com>
2023-11-30 09:06:23 -0300

    Update main.c
    Arrumando problemas de identação

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include "biblioteca.h"
+#include "funcoes.h"
+
+//Ana Carolina Lazzuri RA: 22.123.001-4
 
 int main() {
     STarefas tarefas[100];
     int numero_de_tarefas = Carregar_Tarefas(tarefas);
-
 
     int escolha;
     while(1){
@@ -12,15 +13,9 @@ int main() {
         printf("1. Cadastrar Tarefa\n");
         printf("2. Listar Tarefas\n");
         printf("3. Deletar Tarefa\n");
-        printf("4. Filtrar Tarefas\n");
-        printf("5. Alterar Tarefas\n");
-        printf("6. Exportar Tarefas\n");
-        printf("7. Sair\n");
+        printf("4. Sair\n");
         scanf("%d", &escolha);
 
-        int opcaoExportar;
-        int prioridadeExportar;
-        char categoriaExportar[100];
         switch (escolha) {
             case 1:
                 Cadastrar_Tarefas(tarefas, &numero_de_tarefas);
@@ -32,56 +27,17 @@ int main() {
                 if (numero_de_tarefas > 0) {
                     int priori;
                     char cate[100];
-                    printf("Coloque a prioridade da tarefa que deseja excluir: ");
+                    printf("Prioridade da tarefa que deseja deletar: ");
                     scanf("%d", &priori);
-                    printf("Coloque a categoria da tarefa que deseja excluir: ");
-                    clear_buffer();
+                    printf("Categoria da tarefa que deseja deletar: ");
+                    getchar();
                     fgets(cate, sizeof(cate), stdin);
                     Deletar_Tarefa(tarefas, &numero_de_tarefas, cate, priori);
                 } else {
-                    printf("A lista de tarefas esta vazia.\n");
+                    printf("Lista de tarefas vazia.\n");
                 }
                 break;
             case 4:
-                FiltrarTarefas(tarefas, numero_de_tarefas);
-                break;
-            case 5:
-                AlterarTarefas(tarefas, numero_de_tarefas);
-                break;
-            case 6:
-                printf("Escolha uma opcao de exportacao: ");
-                printf("1. Exportar Tarefas por Prioridade\n");
-                printf("2. Exportar Tarefas por Categoria\n");
-                printf("3. Exportar Tarefas por Prioridade e Categoria\n");
-                scanf("%d", &opcaoExportar);
-
-                switch (opcaoExportar) {
-                    case 1:
-
-                        printf("Digite a prioridade: ");
-                        scanf("%d", &prioridadeExportar);
-                        ExportarTarefasPorPrioridade(tarefas, numero_de_tarefas, prioridadeExportar);
-                        break;
-                    case 2:
-                        printf("Digite a categoria: ");
-                        clear_buffer();
-                        fgets(categoriaExportar, sizeof(categoriaExportar), stdin);
-                        ExportarTarefasPorCategoria(tarefas, numero_de_tarefas, categoriaExportar);
-                        break;
-                    case 3:
-                        printf("Digite a prioridade: ");
-                        scanf("%d", &prioridadeExportar);
-                        printf("Digite a categoria: ");
-                        clear_buffer();
-                        fgets(categoriaExportar, sizeof(categoriaExportar), stdin);
-                        ExportarTarefasPorPrioridadeECategoria(tarefas, numero_de_tarefas, prioridadeExportar, categoriaExportar);
-                        break;
-                    default:
-                        printf("Essa opcao nao existe.\n");
-                        break;
-                }
-                break;
-            case 7:
                 printf("Sessao encerrada\n");
                 break;
             default:
